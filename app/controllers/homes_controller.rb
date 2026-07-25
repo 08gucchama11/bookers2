711cbb946017d0565c33cc4dc04a9fd1da2ff475
08gucchama11 <296365775+08gucchama11@users.noreply.github.com>
2026-07-25 05:12:09 +0000

    [Add]ユーザー認証機能実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,16 @@
 class HomesController < ApplicationController
+  allow_unauthenticated_access only: [:top, :about]
+
   def top
   end
 
   def about
+  end
+
+  private
+
+  def request_authentication
+    session[:return_to_after_authenticating] = request.url
+    redirect_to sign_in_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   root to: "homes#top"
+  get "home/about" => "homes#about"
+
+  # 認証
+  get    "users/sign_in",  to: "sessions#new",     as: :sign_in
+  post   "users/sign_in",  to: "sessions#create"
+  delete "users/sign_out", to: "sessions#destroy", as: :sign_out
+
+  # 新規登録（明示的に定義）
+  get "users/sign_up", to: "users#new", as: :sign_up
+
+  # ユーザー作成・詳細・編集・更新
+  resources :users, only: [:create, :show, :edit, :update]
+
+  # パスワード再設定
+  resources :passwords, only: [:new, :create, :edit, :update], param: :token
+
+  # 投稿機能
+  resources :books
 end
